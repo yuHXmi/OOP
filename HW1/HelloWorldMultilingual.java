f0aa9ee9f9e8194b47de9fe93b31fb42dedd9b5d
XHuy <xhuy8248@gmail.com>
2024-04-08 22:40:02 +0700

    Create HelloWorldMultilingual.java

---
+++ HW1/HelloWorldMultilingual.java
@@ -0,0 +1,9 @@
+public class HelloWorldMultilingual {
+    public static void main (String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Xin chào thế giới!");
+        System.out.println("Privet, mir!");
+        System.out.println("Nǐ hǎo shìjiè!");
+        System.out.println("Kon'nichiwa sekai!");
+    }
+}
