2e4fc0a1998214bf3e2c3db741bb944ec533690b
XHuy <xhuy8248@gmail.com>
2024-04-08 22:39:48 +0700

    Create Main.java

---
+++ HW1/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("HaXuanHuy\t23020375\tK68-AI1\tyuHXmi\txhuy8248@gmail.com");
+        for (int i = 9; i >= 1; i--) {
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.print("No more bottles of beer on the wall.");
+    }
+}
