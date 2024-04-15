9624b5e6a51b11306178953c5d117e9643f86658
XHuy <92810277+yuHXmi@users.noreply.github.com>
2024-04-15 21:34:47 +0700

    Update FlipX.java

--- HW2/FlipX.java
+++ HW2/FlipX.java
@@ -3,16 +3,16 @@ import java.awt.Image;
 
 public class FlipX {
     public static void main (String[] args) {
-//        Picture pic = new Picture(args[0]);
-//        int width = pic.width();
-//        int height = pic.height();
-//        pic.show();
-//        for (int x = 0; x < height; x++)
-//            for (int y = 0; y < width / 2; y++) {
-//                Color temp = pic.get(x, y);
-//                pic.set(x, y, pic.get(x, width - y - 1));
-//                pic.set(x, width - y - 1, temp);
-//            }
-//        pic.show();
+       Picture pic = new Picture(args[0]);
+       int width = pic.width();
+       int height = pic.height();
+       pic.show();
+       for (int x = 0; x < height; x++)
+           for (int y = 0; y < width / 2; y++) {
+               Color temp = pic.get(x, y);
+               pic.set(x, y, pic.get(x, width - y - 1));
+               pic.set(x, width - y - 1, temp);
+           }
+       pic.show();
     }
 }
